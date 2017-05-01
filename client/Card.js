c7e19a4ddb53a2c5960d2cd077b6908fbf54f187
Anwesh43 <anweshthecool0@gmail.com>
2017-05-01 21:33:58 +0530

    Adding fixes in card.js file

--- client/Card.js
+++ client/Card.js
@@ -1,27 +1,39 @@
 class Card {
-    constructor(imagesrc,title,description) {
+    constructor(imagesrc,title,description,color,col=3,row=2) {
         this.imagesrc = imagesrc
+        this.color = color
         this.title = title
         this.description = description
         this.canvas = document.createElement('canvas')
         this.context = this.canvas.getContext('2d')
-        this.canvas.width = window.innerWidth/4
-        this.canvas.height = 4*window.innerHeight/5
+        this.canvas.width = window.innerWidth/col
+        this.canvas.height = window.innerHeight/row
     }
     render(x,y) {
         this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
         const image = new Image()
         image.src = this.imagesrc
-        image.width = this.canvas.width/8
-        image.height = this.canvas.height/8
-        const w = canvas.width,h = canvas.height
-        const imX = this.canvas.width/2,imY = this.canvas.height/10 ,imW = image.width,imH = image.height
+        image.width = this.canvas.height/4
+        image.height = this.canvas.height/4
+        const w = this.canvas.width,h = this.canvas.height
+        const imX = w/2,imY = h/7 ,imW = image.width,imH = image.height
+        const context = this.context
         image.onload = () => {
-            context.drawImage(image,imX-imW/2,imY-imH/2)
-            context.font = context.font.replace(/\d{2}/,20)
-            context.drawText(this.title,w/2-context.measureText(title).width/2,this.canvas.height/6)
+            context.fillStyle = this.color
+            context.fillRect(0,0,w,h)
+            context.fillStyle = 'black'
+            context.save()
+            context.beginPath()
+            context.arc(imX,imY,imW/2,0,2*Math.PI)
+            context.clip()
+            context.fill()
+            context.drawImage(image,imX-imW/2,imY-imH/2,imW,imH)
+            context.restore()
+            context.fillStyle="white"
+            context.font = context.font.replace(/\d{2}/,h/15)
+            context.fillText(this.title,w/2-context.measureText(this.title).width/2,imY+imH/2+imH/3)
             const img = document.createElement('img')
-            img.src = canvas.toDataURL()
+            img.src = this.canvas.toDataURL()
             img.style.position = "absolute"
             img.style.top = y
             img.style.left = x
